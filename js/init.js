1e99f32b6ac64fd43e7ab63dc99d1661bb71beac
samuelMancillaBermudez <82414759+samuelMancillaBermudez@users.noreply.github.com>
2021-04-14 00:17:00 -0500

    Commit final

---
+++ js/init.js
@@ -0,0 +1,9 @@
+// @ts-ignore
+firebase.initializeApp({
+  apiKey: "AIzaSyBIoWGtkjYmK2Mf_rkc5g277_6kSyX4mv8",
+    authDomain: "ctrliotgithubsamuel.firebaseapp.com",
+    projectId: "ctrliotgithubsamuel",
+    storageBucket: "ctrliotgithubsamuel.appspot.com",
+    messagingSenderId: "612536916340",
+    appId: "1:612536916340:web:a3cfbe9035be867aa73421"
+});
